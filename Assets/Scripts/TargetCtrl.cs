b559ef46f2f050d7069436741a055bd3f2e67ef6
noppelmax <max@noppelmax.online>
2017-01-21 17:50:22 +0100

    bootstrap TargetCtrl

--- Assets/Scripts/TargetCtrl.cs
+++ Assets/Scripts/TargetCtrl.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class TargetCtrl : MonoBehaviour {
 
+	private Rigidbody2D rb;
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void FixedUpdate(){
+
+	}
+
 }
