87db1cb00bf7bcb920aa4b69e461c80ee9ce3b86
Florian Schlegel <fschlegel@gmx.net>
2017-01-22 13:26:24 +0100

    Türen und Schalter

---
+++ Assets/Scripts/DoorCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCtrl : MonoBehaviour {
+
+	public float openDuration = 2;
+
+	private bool open = false;
+	private float closeTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SwitchActivated() {
+		//gameObject.SetActive (false);
+		open = true;
+		SpriteRenderer rd = GetComponentInChildren<SpriteRenderer> ();
+		BoxCollider2D collid = GetComponent<BoxCollider2D> ();
+		collid.isTrigger = true;
+		rd.color = new Color (1.0f, 1.0f, 1.0f, 0.1f);
+		closeTime = Time.fixedTime + openDuration;
+		Debug.Log ("Start " + Time.fixedTime + " " + closeTime);
+	}
+
+	void FixedUpdate() {
+		if (open) {
+			if (Time.fixedTime > closeTime) {
+				Debug.Log ("Ablauf");
+				open = false;
+				SpriteRenderer rd = GetComponentInChildren<SpriteRenderer> ();
+				rd.color = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+				BoxCollider2D collid = GetComponent<BoxCollider2D> ();
+				collid.isTrigger = false;
+				//gameObject.SetActive (true);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GridCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCtrl : MonoBehaviour {
+	//public GameObject 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		// TODO Play sound depending on what happens. Destroyed, Glas, ...
+		//AudioSource.PlayClipAtPoint (bounceSound, new Vector3 (0, 0, 0));
+
+		Debug.Log ("Triggered!");
+
+		if (col.gameObject.name.Equals("Projektil(Clone)")) {
+			ProjektilCtrl pro = col.gameObject.GetComponent<ProjektilCtrl> ();
+			Rigidbody2D rig = pro.GetComponent<Rigidbody2D> ();
+			Debug.Log ("passed through: " + rig.velocity);
+			Vector2 vel = rig.velocity;
+			float speed = vel.magnitude;
+			Vector2 dir = vel.normalized;
+
+			GameObject projektil = (GameObject)Instantiate (col.gameObject);
+			Rigidbody2D rig2 = projektil.GetComponent<Rigidbody2D> ();
+
+			rig.velocity = Quaternion.Euler (0, 0, -30) * dir * speed;
+			rig2.velocity = Quaternion.Euler (0, 0, 30) * dir * speed;
+		}
+	}
+
+}
